9fd7d2d7d24987b9f377815c7747809f741e40e6
Pawel <ppazyrski@wp.pl>
2026-07-09 18:28:38 +0200

    implement user creation and password hashing in signup function

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -2,6 +2,7 @@
 
 import { db } from "@/db";
 import { employees } from "@/db/schema";
+import bcrypt from "bcryptjs";
 import { eq } from "drizzle-orm";
 
 export async function signup(prevState: any, formData: FormData) {
@@ -12,20 +13,44 @@ export async function signup(prevState: any, formData: FormData) {
 
   const errors: Record<string, string> = {};
 
-  if (!firstName || firstName.trim() === "") errors.firstName = "Imię jest wymagane.";
-  if (!lastName || lastName.trim() === "") errors.lastName = "Nazwisko jest wymagane.";
-  if (!email || !email.includes('@')) errors.email = "Niepoprawny adres e-mail.";
-  if (!password || password.trim().length < 8) errors.password = "Hasło musi mieć co najmniej 8 znaków.";
+  if (!firstName || firstName.trim() === "")
+    errors.firstName = "Imię jest wymagane.";
+  if (!lastName || lastName.trim() === "")
+    errors.lastName = "Nazwisko jest wymagane.";
+  if (!email || !email.includes("@"))
+    errors.email = "Niepoprawny adres e-mail.";
+  if (!password || password.trim().length < 8)
+    errors.password = "Hasło musi mieć co najmniej 8 znaków.";
 
   if (Object.keys(errors).length > 0) {
-    return errors
+    return errors;
   }
 
-  const existingUser = await db.query.employees.findFirst({
-    where: eq(employees.email, email),
-  })
+  try {
+    const existingUser = await db.query.employees.findFirst({
+      where: eq(employees.email, email),
+    });
 
-  if (existingUser) {
-    return  {errors: {email: "Konto z tym adresem e-mail już istnieje."}}
+    if (existingUser) {
+      return { errors: { email: "Konto z tym adresem e-mail już istnieje." } };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await db.insert(employees).values({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    return {
+      success: true,
+      message: "Konto zostało poprawnie utworzone!",
+    };
+  } catch (error) {
+    return {
+      errors: { general: "Wystąpił błąd podczas zapisywania." },
+    };
   }
 }
